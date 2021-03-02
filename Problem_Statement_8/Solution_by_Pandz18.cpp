1ae1589831274e680b18eee47230509fa10656bf
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-02 16:43:59 +0530

    Create Solution_by_Pandz18.cpp

---
+++ Problem_Statement_8/Solution_by_Pandz18.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include<iostream>
+using namespace std;
+
+int lucas(int n) 
+{ 
+
+    if (n == 0) 
+        return 2; 
+    if (n == 1) 
+        return 1; 
+  
+
+    return lucas(n - 1) +  
+           lucas(n - 2); 
+} 
+  
+
+int main() 
+{ 
+    int n ;
+    cout<<"enter the position of the element of the series ";
+    cin>>n;
+    printf("%d", lucas(n)); 
+    return 0; 
+} 
