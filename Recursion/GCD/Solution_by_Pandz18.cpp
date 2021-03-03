796c0f774b0c68f384c7574eaae5a63437589b89
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-03 08:51:27 +0530

    Create Solution_by_Pandz18.cpp

---
+++ Recursion/GCD/Solution_by_Pandz18.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, GCD;
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+
+    
+    if ( n2 > n1) {   
+        int temp = n2;
+        n2 = n1;
+        n1 = temp;
+    }
+    
+    for (int i = 1; i <=  n2; ++i) {
+        if (n1 % i == 0 && n2 % i ==0) {
+            GCD = i;
+        }
+    }
+
+    cout << "GCD = " << hcf;
+    return 0;
+}
