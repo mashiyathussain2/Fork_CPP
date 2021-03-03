20feffd57005d781676460e8ab1abe4db404c580
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-03 09:18:21 +0530

    Update Solution_by_Pandz18.cpp

--- Recursion/GCD/Solution_by_Pandz18.cpp
+++ Recursion/GCD/Solution_by_Pandz18.cpp
@@ -7,19 +7,33 @@ int main()
     cout << "Enter two numbers: ";
     cin >> n1 >> n2;
 
-    
-    if ( n2 > n1) {   
+    if(n1>0 && n2>0)
+    {
+        if ( n2 > n1 ) {   
         int temp = n2;
         n2 = n1;
         n1 = temp;
-    }
+        }
     
     for (int i = 1; i <=  n2; ++i) {
         if (n1 % i == 0 && n2 % i ==0) {
             GCD = i;
         }
     }
-
-    cout << "GCD = " << GCD;
+      cout << "GCD of the two numbers is : " << GCD;
+    }
+    else if(n1==0 || n2==0)
+    {
+        cout<<max(n1, n2);
+    }
+    else if( n1==0 && n2==0)
+    {
+        cout<<"0";
+    }
+    else
+    {
+        cout<<"invalid";
+    }
+  
     return 0;
 }
