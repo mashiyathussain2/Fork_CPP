944e1b3c7e51427a3f38b873b8501cf90758808a
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 01:26:49 +0530

    Create Solution_by_Pandz18.cpp

---
+++ CodeChef/Practice Solutions/Chef And Operators/Solution_by_Pandz18.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t , a , b;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b;
+	    if(a>b)
+	    {
+	        cout<<" > "<<endl;
+	    
+	     } 
+	    else if(a<b)
+	    {
+	        cout<<"< "<<endl;
+	    }
+	    else
+	    {
+	        cout<<"= "<<endl;
+	    }
+	}
+	return 0;
+}
