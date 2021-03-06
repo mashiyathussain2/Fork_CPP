86335e60f1ef30e8ed97babd5c9716020887389d
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 01:15:22 +0530

    Create Solution_by_Pandz18.cpp

---
+++ CodeChef/Practice Solutions/Second Largest/Solution_by_Pandz18.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	long long int T;
+	cin>>T;
+	while(T--)
+	{
+	long long int A,B,C ;
+	cin>>A>>B>>C;
+	if(A>=B&&A>=C)
+	{
+		if(B>=C)
+		cout<<B<<endl;
+		else
+		cout<<C<<endl;
+	}
+	else if(B>=A&&B>=C)
+    	{
+		   if(A>=C)
+		   cout<<A<<endl;
+		   else
+		   cout<<C<<endl;
+	    }
+	else
+	{
+	   if(A>=B)
+	   cout<<A<<endl;
+	   else
+	   cout<<B<<endl;    
+    }
+	}
+	return 0;
+}
