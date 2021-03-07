0cd701ca3a97df2d58ed0d75121d89d93162c478
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 11:30:18 +0530

    Create Solution_by_Pandz18.cpp

---
+++ CodeChef/Practice Solutions/Decrement OR Increment/Solution_by_Pandz18.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n;
+
+	    cin>>n;
+	    if(n%4==0)
+	    {
+	        cout<<n+1<<endl;
+	    }
+	    else
+	    {
+	        cout<<n-1<<endl;
+	    }
+	
+	return 0;
+}
