030c316ea64475c2c8af385caa24837b9d7fc6b7
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-04-02 20:51:07 +0530

    Create Solution_1.cpp

---
+++ Array/Sorting/InsertionSort/Solution_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+
+int main() {
+	
+    int arr[]={50,20,40,60,10,30};
+	
+	int n=sizeof(arr)/sizeof(arr[0]);
+	insertionSort(arr,n);
+	
+	for(auto x: arr)
+	    cout<<x<<" ";
+}
