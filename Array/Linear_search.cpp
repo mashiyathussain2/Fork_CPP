a9c56900a4830b7e013a303d5046a003d5e48288
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-26 18:00:48 +0530

    Update Linear_search.cpp
    declared i out of loop and asked user to input size of array

--- Array/Linear_search.cpp
+++ Array/Linear_search.cpp
@@ -1,15 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int A[10],n=10,key;
+int main()
+{
+    int A[10] , n , key , i;
+    
+    cout<<"Enter the number of elements of an array "<<endl;
+    cin>>n;
+    cout<<" "<<endl;
+    
     cout<<"Enter the elements of an array "<<endl;
-    for (int i = 1; i <n; i++)
+    for (i = 1; i <n; i++)
     {
         cin>>A[i];
     }
+    
     cout<<"Enter the element to search "<<endl;
     cin>>key;
-    for (int i = 1; i <n; i++)
+    for (i = 1; i <n; i++)
     {
         if (key==A[i])
         {
@@ -20,6 +27,4 @@ int main(){
     }
     cout<<"Not found"<<endl;
     return 0;
-    
-    
-}
+    }
