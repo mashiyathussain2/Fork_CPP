b23ba66848c8a952d81312d77f92570f8a3b35ef
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-04-27 11:10:19 +0530

    Create Sqrt(x).cpp

---
+++ Sqrt(x).cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , sq;
+    
+    cin>>a;
+    sq= sqrt(a);
+    cout<<sq;
+    return 0;
+}
