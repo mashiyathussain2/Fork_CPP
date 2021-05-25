b3e30348fe3337a67036929b419822763a6d9b83
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-05-25 20:57:01 +0530

    Create Solution_1.cpp

---
+++ Bit_Manipulation/Find_Unique_Number/Solution_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int no;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        cin>>no;
+        ans=ans^no;
+    }
+    cout<<ans<<endl;
+}
