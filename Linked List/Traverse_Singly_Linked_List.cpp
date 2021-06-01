8df96c4836d4f47437a1b07e19d757a223e1a0f9
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-02 00:33:47 +0530

    Create Traverse_Singly_Linked_List.cpp

---
+++ Linked List/Traverse_Singly_Linked_List.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=new Node(40);
+	printlist(head);
+	return 0;
+} 
