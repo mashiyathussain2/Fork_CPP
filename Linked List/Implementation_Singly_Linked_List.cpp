e4e81da736098a655351577e291d2d1aab092e4f
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-02 00:32:25 +0530

    Create Implementation_Singly_Linked_List.cpp

---
+++ Linked List/Implementation_Singly_Linked_List.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	Node *temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->next=temp2;
+	cout<<head->data<<"-->"<<temp1->data<<"-->"<<temp2->data;
+	return 0;
+} 
