820d48b6bedd2f43072c8bbd31751349ec803e1d
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-06 14:14:34 +0530

    Create Naive_Solution.cpp

---
+++ Linked List/Circular Linked List/Insert_At_Begin/Naive_Solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    if(head==NULL)return;
+    Node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+Node *insertBegin(Node * head,int x){
+    Node *temp=new Node(x);
+    if(head==NULL)
+        temp->next=temp;
+    else{
+        Node *curr=head;
+        while(curr->next!=head)
+            curr=curr->next;
+        curr->next=temp;
+        temp->next=head;
+    }
+    return temp;
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=head;
+	head=insertBegin(head,15);
+	printlist(head);
+	return 0;
+} 
