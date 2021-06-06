27a5823444db8275da346198ee8f563945008d06
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-06 14:13:26 +0530

    Create Do_while_method.cpp

---
+++ Linked List/Circular Linked List/Traversal/Do_while_method.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    if(head==NULL)return;
+    Node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(5);
+	head->next->next=new Node(20);
+	head->next->next->next=new Node(15);
+	head->next->next->next->next=head;
+	printlist(head);
+	return 0;
+} 
