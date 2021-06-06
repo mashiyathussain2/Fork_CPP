d9655a7cdcb1c966270fab850451b76c6dd084cc
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-06 14:07:14 +0530

    Create Implementation.cpp

---
+++ Linked List/Doubly Linked List/Implementation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* prev;
+    Node* next;
+    Node(int d){
+        data=d;
+        prev=NULL;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }cout<<endl;
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	Node *temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->prev=head;
+	temp1->next=temp2;
+	temp2->prev=temp1;
+	printlist(head);
+	return 0;
+} 
