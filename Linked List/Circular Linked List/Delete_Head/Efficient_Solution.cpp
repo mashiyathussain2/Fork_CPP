9119baa15724e005fd708076f4fe1465d39c6035
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-06 14:19:06 +0530

    Create Efficient_Solution.cpp

---
+++ Linked List/Circular Linked List/Delete_Head/Efficient_Solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    if(head==NULL)return;
+    Node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+Node *delHead(Node *head){
+    if(head==NULL)return NULL;
+    if(head->next==head){
+        delete head;
+        return NULL;
+    }
+    head->data=head->next->data;
+    Node *temp=head->next;
+    head->next=head->next->next;
+    delete temp;
+    return head;
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=new Node(40);
+	head->next->next->next->next=head;
+	head=delHead(head);
+	printlist(head);
+	return 0;
+} 
