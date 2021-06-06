b3c88f62b263c118b56e4aa68a82133075a40cdb
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-06 14:15:39 +0530

    Create Efficient_Solution.cpp

---
+++ Linked List/Circular Linked List/Insert_At_Begin/Efficient_Solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void printlist(Node *head){
+    if(head==NULL)return;
+    Node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+Node *insertBegin(Node * head,int x){
+    Node *temp=new Node(x);
+    if(head==NULL){
+        temp->next=temp;
+        return temp;    
+    }
+    else{
+        temp->next=head->next;
+        head->next=temp;
+        int t=head->data;
+        head->data=temp->data;
+        temp->data=t;
+        return head;
+    }
+}
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	head->next->next->next=head;
+	head=insertBegin(head,15);
+	printlist(head);
+	return 0;
+} 
