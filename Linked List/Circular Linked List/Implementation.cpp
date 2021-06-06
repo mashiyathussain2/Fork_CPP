1e648738beb1652dfa1cf30c895f88c4de37cb8f
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-06 14:11:54 +0530

    Create Implementation.cpp

---
+++ Linked List/Circular Linked List/Implementation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(5);
+	head->next->next=new Node(20);
+	head->next->next->next=new Node(15);
+	head->next->next->next->next=head;
+	return 0;
+} 
