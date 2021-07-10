c4529519c00df269218cdc32ca7981b0ab6dfdf5
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-07-10 21:03:14 +0530

    Create Preorder_Build_And_Print.cpp

---
+++ Binary Tree/Preorder_Build_And_Print.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+
+    if(d==-1){
+        return NULL;
+    }
+    node * root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+void print(node *root){
+    if(root==NULL){
+        return;
+    }
+    //Otherwise, print root first followed by subtrees(children)
+    cout<<root->data<<" ";
+    print(root->left);
+    print(root->right);
+}
+int main(){ 
+    node* root = buildTree();
+    print(root);
+
+
+return 0;
+}
