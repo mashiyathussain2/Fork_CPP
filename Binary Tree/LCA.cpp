f97ae853385e9b371e4b5ab50b0d817af8969956
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-07-15 13:29:17 +0530

    Create LCA.cpp

---
+++ Binary Tree/LCA.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+Node *lca(Node *root, int n1, int n2){
+    if(root==NULL)return NULL;
+    if(root->key==n1||root->key==n2)
+        return root;
+    
+    Node *lca1=lca(root->left,n1,n2);
+    Node *lca2=lca(root->right,n1,n2);
+    
+    if(lca1!=NULL && lca2!=NULL)
+        return root;
+    if(lca1!=NULL)
+        return lca1;
+    else
+        return lca2;
+}
+
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	int n1=20,n2=50;
+	
+	Node *ans=lca(root,n1,n2);
+	cout<<"LCA: "<<ans->key;
+}
