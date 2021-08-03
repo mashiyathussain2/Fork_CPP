ad9183df66ff5849323572a918afabaa6dd9b2b4
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-08-04 01:00:58 +0530

    Create Strings in STL.cpp

---
+++ STL/Strings in STL.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s0;
+    string s1("Hello World");
+    string s2 = "Hello World";
+    string s3(s2);
+    string s4 = s3;
+    char a[] ={'a','b','c','\0'};
+    string s5(a);
+
+    cout<<s0<<endl;
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+    cout<<s3<<endl;
+    cout<<s4<<endl;
+    cout<<s5<<endl;
+}
