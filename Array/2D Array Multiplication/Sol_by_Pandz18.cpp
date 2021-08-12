3b07f90904ebdc89923d969f4274545fab7ddfc7
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-08-13 00:32:10 +0530

    Update

--- Array/2D Array Multiplication/Sol_by_Pandz18.cpp
+++ Array/2D Array Multiplication/Sol_by_Pandz18.cpp
@@ -21,8 +21,6 @@ int main()
         cout << "Enter rows and columns for second matrix: ";
         cin >> r2 >> c2;
     }
-
-
     cout << endl << "Enter elements of matrix 1:" << endl;
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c1; ++j)
@@ -30,8 +28,6 @@ int main()
             cout << "Enter element a" << i + 1 << j + 1 << " : ";
             cin >> a[i][j];
         }
-
-
     cout << endl << "Enter elements of matrix 2:" << endl;
     for(i = 0; i < r2; ++i)
         for(j = 0; j < c2; ++j)
@@ -39,22 +35,17 @@ int main()
             cout << "Enter element b" << i + 1 << j + 1 << " : ";
             cin >> b[i][j];
         }
-
-
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c2; ++j)
         {
             mult[i][j]=0;
         }
-
-
     for(i = 0; i < r1; ++i)
         for(j = 0; j < c2; ++j)
             for(k = 0; k < c1; ++k)
             {
                 mult[i][j] += a[i][k] * b[k][j];
             }
-
 
     cout << endl << "Output Matrix: " << endl;
     for(i = 0; i < r1; ++i)
