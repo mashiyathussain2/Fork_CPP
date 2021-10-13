d08023b4041443b5cd9b3a23848e38fd8262007c
Pandz18 <panda@johnson.in>
2021-10-13 19:01:34 +0530

    Create Sol.cpp

---
+++ Array/Product Of Two Arrays/Sol.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n, A[100] , B[100];
+  cin>>n;
+  for(int i=0 ; i<n ; i++)
+  {
+    cin>>A[i];
+  }
+  for(int j=0 ; j<n ; j++)
+  {
+    cin>>B[j];
+  }
+  
+  for(int k=0 ; k<n ; k++)
+  {
+    cout<<"A["<<k<<"]"<<""<<"x"<<" "<<"B["<<k<<"] = "<<(A[k]*B[k])<<endl;
+  }
+                                                        
+  return 0;
+}
